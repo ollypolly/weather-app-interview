96c0240ae896540ddc4a005c51bd388cdc30bdae
Olly Pethick <ollypethick@gmail.com>
2024-11-19 14:08:26 +0000

    First four answers

--- App.tsx
+++ App.tsx
@@ -1,35 +1,72 @@
 import { StatusBar } from "expo-status-bar";
 import { useEffect, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  ActivityIndicator,
+  Button,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import "react-native-url-polyfill/auto";
-import { server } from "./api";
+import { server, WeatherData } from "./api";
 
 // don't worry about this, it's just setting up the mock api server
 server.listen({ onUnhandledRequest: "bypass" });
 
 export default function App() {
-  const [data, setData] = useState();
+  const [data, setData] = useState<WeatherData>();
+
+  const [city, setCity] = useState<string>("london");
 
   useEffect(() => {
-    fetch("https://localhost:3000/london")
+    setData(undefined);
+    fetch(`https://localhost:3000/${city}`)
       .then((response) => response.json())
       .then(setData)
       .catch(console.error);
-  }, []);
+  }, [city]);
 
   return (
-    <View style={styles.container}>
-      <Text>{JSON.stringify(data)}</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <ScrollView contentContainerStyle={styles.container}>
+        <Button title="London" onPress={() => setCity("london")} />
+        <Button title="Sydney" onPress={() => setCity("sydney")} />
+        {data ? <CountryCard {...data} /> : <ActivityIndicator />}
+        <StatusBar style="auto" />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
+
+const CountryCard = (data: WeatherData) => {
+  return (
+    <View style={styles.countryCard}>
+      <Text>City</Text>
+      <Text>{data.city}</Text>
+      <Text>Summary</Text>
+      <Text>{data.summary}</Text>
+      <Text>Max Temperature</Text>
+      <Text>{data.maxTemperature}</Text>
+      <Text>Min Temperature</Text>
+      <Text>{data.minTemperature}</Text>
+      <Text>Wind Speed</Text>
+      <Text>{data.windSpeed}</Text>
+      <Text>Wind Direction</Text>
+      <Text>{data.windDirection}</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "flex-start",
+  },
+  countryCard: {
+    margin: 20,
   },
 });
--- api.ts
+++ api.ts
@@ -1,7 +1,7 @@
 import { rest } from "msw";
 import { setupServer } from "msw/native";
 
-interface WeatherData {
+export interface WeatherData {
   city: string;
   summary: string; // "Sunny", "Cloudy", "Rainy"
   maxTemperature: number;
@@ -10,37 +10,39 @@ interface WeatherData {
   windDirection: string; // "N", "S", "E", "W"
 }
 
+const london = {
+  city: "London",
+  summary: "Cloudy",
+  maxTemperature: 20,
+  minTemperature: 10,
+  windSpeed: 15,
+  windDirection: "E",
+};
+
+const sydney = {
+  city: "Sydney",
+  summary: "Sunny",
+  maxTemperature: 40,
+  minTemperature: 30,
+  windSpeed: 5,
+  windDirection: "W",
+};
+
 const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const handlers = [
   rest.get("https://localhost:3000/london", async (_, res, ctx) =>
-    res(
-      ctx.status(200),
-      ctx.json<WeatherData>({
-        city: "London",
-        summary: "Cloudy",
-        maxTemperature: 20,
-        minTemperature: 10,
-        windSpeed: 15,
-        windDirection: "E",
-      })
-    )
+    res(ctx.status(200), ctx.json<WeatherData>(london))
   ),
 
   rest.get("https://localhost:3000/sydney", async (_, res, ctx) => {
     await wait(2000);
 
-    return res(
-      ctx.status(200),
-      ctx.json<WeatherData>({
-        city: "Sydney",
-        summary: "Sunny",
-        maxTemperature: 40,
-        minTemperature: 30,
-        windSpeed: 5,
-        windDirection: "W",
-      })
-    );
+    return res(ctx.status(200), ctx.json<WeatherData>(sydney));
+  }),
+
+  rest.get("https://localhost:3000/cities", async (_, res, ctx) => {
+    return res();
   }),
 ];
 
